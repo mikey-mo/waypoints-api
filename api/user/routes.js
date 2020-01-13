bde434d0647d3af32e1ed944a30dc292fc771e28
Michael Monahan <mikey@jigsaw.xyz>
2020-01-13 18:06:47 -0500

    add prettifier

--- api/user/routes.js
+++ api/user/routes.js
@@ -3,13 +3,29 @@ const userServices = require("../../services/user-services");
 
 theFramework.post(
   "/user/login",
-  [],
+  [
+    {
+      id: "password",
+      type: theFramework.STRING,
+      required: true,
+      description: "User password"
+    },
+    {
+      id: "email",
+      type: theFramework.STRING,
+      required: true,
+      description: "User email"
+    }
+  ],
   {
     description: "Logs in a user",
-    authRequired: false,
+    authRequired: false
   },
-  async params => ({ message: 'Log In Hello' }),
-)
+  async params => {
+    console.log(params);
+    return userServices.login(params.email, params.password);
+  }
+);
 
 theFramework.post(
   "/user/register",
@@ -37,18 +53,18 @@ theFramework.post(
       type: theFramework.STRING,
       required: true,
       description: "Password"
-    },
+    }
   ],
   {
     description: "Registers a user",
-    authRequired: false,
+    authRequired: false
   },
   async params => {
     return userServices.register(
       params.first_name,
       params.last_name,
       params.email,
-      params.password,
+      params.password
     );
-  },
-)
+  }
+);
--- services/user-services.js
+++ services/user-services.js
@@ -2,22 +2,14 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const databaseServices = require("./database-services");
 const { USERS_TABLE } = require("./database/types");
-const CLEAN_FIELDS = [
-  "id",
-  "first_name",
-  "last_name",
-  "email",
-];
+const CLEAN_FIELDS = ["id", "first_name", "last_name", "email"];
 const SALT_SIZE = 10;
 
 module.exports = {
-  async register(
-    firstName,
-    lastName,
-    email,
-    password,
-  ) {
-    const existingUser = await databaseServices.queryForOne(USERS_TABLE, { email });
+  async register(firstName, lastName, email, password) {
+    const existingUser = await databaseServices.queryForOne(USERS_TABLE, {
+      email
+    });
 
     if (existingUser) throw "Email already registered";
 
@@ -25,8 +17,29 @@ module.exports = {
       first_name: firstName,
       last_name: lastName,
       email: email.toLowerCase(),
-      password: this.hashPassword(password),
+      password: this.hashPassword(password)
     });
+
+    const cleanUser = this.cleanUser(user);
+
+    return {
+      user: cleanUser,
+      token: this.createJwt(cleanUser)
+    };
+  },
+  async login(email, password) {
+    const user = await databaseServices.queryForOne(
+      USERS_TABLE,
+      {
+        email: email.toLowerCase()
+      },
+      "Invalid email address"
+    );
+
+    // Check password
+    if (!this.checkPassword(password, user.password)) {
+      throw "Invalid password";
+    }
 
     const cleanUser = this.cleanUser(user);
 
@@ -37,6 +50,9 @@ module.exports = {
   },
   hashPassword(password) {
     return bcrypt.hashSync(password, SALT_SIZE);
+  },
+  checkPassword(password, hash) {
+    return bcrypt.compareSync(password, hash);
   },
   cleanUser(user) {
     let cleanUser = {};
@@ -78,5 +94,5 @@ module.exports = {
   },
   hashPassword(password) {
     return bcrypt.hashSync(password, SALT_SIZE);
-  },
-}
+  }
+};
