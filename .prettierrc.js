3f6ad1cc975f38b327c626c63813e978d8c00642
Michael Monahan <mikey@jigsaw.xyz>
2020-01-13 18:12:02 -0500

    more stuff

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-    trailingComma: 'es5',
-    tabWidth: 4,
-    semi: false,
-    singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  noUnusedVars: true,
 }
